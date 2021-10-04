7ee7e5e81af6c4b2e9e43025c7cad3d1148a3173
Maganiczny <sirmagan@gmail.com>
2021-10-04 20:47:37 +0200

    support other buttons

---
+++ Button/Button.php
@@ -0,0 +1,13 @@
+<?php
+	
+	namespace ForMold\Button;
+	
+	class Button extends \ForMold\Button {
+	
+		public $type 		= 'button';
+		
+		public $bootstrap4ClassInput		= ['btn','btn-primary'];
+
+	}
+	
+?>
---
+++ Button/Reset.php
@@ -0,0 +1,13 @@
+<?php
+	
+	namespace ForMold\Button;
+	
+	class Reset extends \ForMold\Button {
+	
+		public $type 		= 'reset';
+		
+		public $bootstrap4ClassInput		= ['btn','btn-primary'];
+
+	}
+	
+?>
